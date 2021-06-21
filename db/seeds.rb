0ea79602f6bc78cb63977b2ed3619cb9c231dfd2
Thierry Tranchant <thierry.tcht@gmail.com>
2021-06-21 17:24:20 +0200

    added jobs for creating participants and reseting draws until qualified players and luckylosers are known

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 array = [[1, 'First'], [2, 'Second'], [3, 'Third'], [4, 'Fourth'], [5, 'Fifth'], [6, 'Sixth'], [7, 'Seventh'], [8, 'Eighth'], [9, 'Ninth'], [10, 'Tenth']]
 
 (1..10).to_a.each do |num|
-  Tennisplayer.create(first_name: array[num - 1][1], last_name: 'Qualified')
-  Tennisplayer.create(first_name: array[num - 1][1], last_name: 'Lucky')
+  Tennisplayer.create(first_name: 'Qualified', last_name: array[num - 1][1], tennisplayer_url: 'Qualified')
+  Tennisplayer.create(first_name: 'Lucky', last_name: array[num - 1][1], tennisplayer_url: 'Lucky')
 end
---
+++ test/jobs/reset_participants_while_qualified_and_luckyloser_in_draw_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ResetParticipantsWhileQualifiedAndLuckyloserInDrawJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
