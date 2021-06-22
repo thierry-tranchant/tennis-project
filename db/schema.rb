d3eb5a2bb3e0d2eda4e9a5328b8dc2f346b4c89d
Thierry Tranchant <thierry.tcht@gmail.com>
2021-06-22 17:25:19 +0200

    added atp and race ranking scrapping logic and scrapped ; also added main colors and font

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_101644) do
+ActiveRecord::Schema.define(version: 2021_06_21_174027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_101644) do
     t.bigint "tennisplayer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
     t.index ["tennisplayer_id"], name: "index_rankings_on_tennisplayer_id"
   end
 
