2573917bbc22b9ac54ed628b064b2c3f3ce233d3
Thierry Tranchant <thierry.tcht@gmail.com>
2021-06-23 11:05:04 +0200

    corrected cron expressions and bugs in jobs

--- app/jobs/fill_games_results_for_current_tournaments_job.rb
+++ app/jobs/fill_games_results_for_current_tournaments_job.rb
@@ -3,6 +3,12 @@ class FillGamesResultsForCurrentTournamentsJob < ApplicationJob
 
   def perform
     scrapps = Scrapp.where(state: 'current', drawed: true)
-    scrapps.each { |scrapp| scrapp.fill_games_results("https://www.atptour.com/en/scores/current/#{scrapp.tournament_location}/#{scrapp.tournament_number}/results") }
+
+    scrapps.each do |scrapp|
+      next if scrapp.participants.map { |participant| participant.tennisplayer.first_name }.any? { |name| ['Qualified', 'Lucky'].include?(name) }
+
+      puts "Filling games results for #{scrapp.tournament_name}..."
+      scrapp.fill_games_results("https://www.atptour.com/en/scores/current/#{scrapp.tournament_location}/#{scrapp.tournament_number}/results")
+    end
   end
 end
