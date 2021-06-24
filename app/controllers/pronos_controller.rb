12f887603733a0c2631403857144cbd81b823546
Thierry Tranchant <thierry.tcht@gmail.com>
2021-06-24 15:00:00 +0200

    created routes, controller and policy for pronos

---
+++ app/controllers/pronos_controller.rb
@@ -0,0 +1,5 @@
+class PronosController < ApplicationController
+  def index
+
+  end
+end
---
+++ app/policies/prono_policy.rb
@@ -0,0 +1,7 @@
+class PronoPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
